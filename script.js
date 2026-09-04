7d6ded238fcf22f4ab7d09ca8e8ada56a28ac725
shena2209 <c20245484.develos@csav.edu.ph>
2026-09-04 14:42:10 +0800

    variable (let and const)

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello, World!");
+console.log("From script.js file.");
+
+const myName = "shena";
+let age = 20;
+let number = 9072744901;
+let address = "saravia";
+
+
+console.log(`Name: ${myName}`);
+console.log(`Age: ${age}`);
+console.log(`Number: ${number}`);
+console.log(`Address: ${address}`);
+console.log('67');
+
